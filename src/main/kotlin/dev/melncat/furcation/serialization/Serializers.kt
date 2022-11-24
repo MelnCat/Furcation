bcfd37fe5f64ca93fb08909ac4e09d9f8da913ca
MelnCat <melncatuwu@gmail.com>
2022-11-24 09:35:33 -0800

    fix

--- src/main/kotlin/dev/melncat/furcation/serialization/Serializers.kt
+++ src/main/kotlin/dev/melncat/furcation/serialization/Serializers.kt
@@ -8,8 +8,8 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.util.*
 
-public object UUIDStringSerializer: KSerializer<UUID> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("java.util.UUID", PrimitiveKind.STRING)
-    override fun serialize(encoder: Encoder, value: UUID): Unit = encoder.encodeString(value.toString())
-    override fun deserialize(decoder: Decoder): UUID = UUID.fromString(decoder.decodeString())
+public object UUIDStringSerializer : KSerializer<UUID> {
+	override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("java.util.UUID", PrimitiveKind.STRING)
+	override fun serialize(encoder: Encoder, value: UUID): Unit = encoder.encodeString(value.toString())
+	override fun deserialize(decoder: Decoder): UUID = UUID.fromString(decoder.decodeString())
 }
